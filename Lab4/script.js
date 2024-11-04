2df393308ca1ee4dc192d1ce594658a10cd0d863
DeVonte Gooden <dg208804@gmail.com>
2024-11-03 23:38:44 -0500

    added some files and tested some js

---
+++ Lab4/script.js
@@ -0,0 +1,19 @@
+const firstName = "DeVonte"
+const lastName = "Gooden"
+const fullName = firstName + " " + lastName
+console.log(fullName);
+
+let population = 633366;
+console.log(population);
+console.log(population * 3);
+
+let javaScriptIsCool = true;
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+for (let i = 1; i < 6; i++) {
+    console.log(i);
+} 
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
