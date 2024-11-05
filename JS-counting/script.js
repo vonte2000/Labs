e357109634213bd19dd1071f2334c21afb10f896
DeVonte Gooden <dg208804@gmail.com>
2024-11-04 21:45:30 -0500

    added js to counting lab

---
+++ JS-counting/script.js
@@ -0,0 +1,33 @@
+console.log("Part 1");
+for (i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+console.log("Part 2");
+for (i = 10; i >= 1; i--) {
+    console.log(i);
+}
+
+console.log("Part 3")
+let j = 1;
+while (j <= 10) {
+    console.log(j);
+    j++;
+}
+j = 10;
+while (j >= 1) {
+    console.log(j);
+    j--;
+}
+
+console.log("Part 4")
+let k = 1
+do {
+    console.log(k);
+    k++;
+} while (k <= 10);
+k = 10;
+do {
+    console.log(k);
+    k--;
+} while (k >= 1)
