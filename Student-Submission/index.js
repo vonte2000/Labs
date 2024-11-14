ad107886501e0a462ea5379e17f8c0840bcc435b
DeVonte Gooden <dg208804@gmail.com>
2024-11-14 01:22:20 -0500

    changed some files and functions

---
+++ Student-Submission/index.js
@@ -0,0 +1,14 @@
+const submissions = [
+    {name: "Jane", score: 95, passed: true},
+    {name: "Joe", score: 77, passed: true},
+    {name: "Jack", score: 59, passed: false},
+    {name: "Jill", score: 88, passed: true}
+];
+
+const addSubmission = (array, newName, newScore) => {
+    const newSubmission = {name: "Te", score: 80},
+    if (score >= 65) {
+        addSubmission.push("Te", 80, true),
+    }
+    
+}
