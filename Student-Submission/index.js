6ae82c471c301dedb30b8b4ca8974f146aa6fee6
DeVonte Gooden <dg208804@gmail.com>
2024-11-18 10:25:20 -0500

    added functionality up to number 5

--- Student-Submission/index.js
+++ Student-Submission/index.js
@@ -6,9 +6,39 @@ const submissions = [
 ];
 
 const addSubmission = (array, newName, newScore) => {
-    const newSubmission = {name: "Te", score: 80},
-    if (score >= 65) {
-        addSubmission.push("Te", 80, true),
+    const submission = {name: newName, score: newScore}
+    if (newScore >= 60) {
+        submission.passed = true 
+    } else {
+        submission.passed = false
     }
-    
+    array.push(submission);
 }
+addSubmission(submissions, "Vonte", 90)
+console.log(submissions)
+
+const deleteSubmissionByIndex = (array, index) => {
+    return submissions.splice(array, index)
+}
+deleteSubmissionByIndex(submissions, 1);
+console.log(submissions);
+
+const deleteSubmissionByName = (array, name) => {
+    //finds index of object with given name
+    const index = array.findIndex(obj => obj.name === name);
+
+    //if object is found, remove it
+    if (index !== -1) {
+        submissions.splice(index, 1);
+    }
+}
+deleteSubmissionByName(submissions, "Jack")
+console.log(submissions);
+
+const editSubmission = (array, index, score) => {
+    const indexToUpdate = [];
+    const newScore = 0;
+    submissions.score == newScore;
+   
+}
+console.log(editSubmission(submissions, 2, 88));
