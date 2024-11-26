375f7f48cb26265e8895912779f45448a56493d5
DeVonte Gooden <dg208804@gmail.com>
2024-11-25 20:55:39 -0500

    added functionality to each element in lab

--- Ready-Set-Action/index.js
+++ Ready-Set-Action/index.js
@@ -4,4 +4,27 @@ const main = () => {
 
     const shrink = document.getElementById("shrink-me");
     shrink.classList.remove("big");
+
+    const listItems = document.querySelectorAll("li");
+    listItems.forEach(item => {
+        console.log(item);
+    })
+
+    const link = document.querySelector("a");
+    link.href = "https://www.example.com";
+    link.textContent = "somewhere";
+    
+    const hide = document.getElementById("hide-me");
+    hide.style.display = 'none';
+
+    const show = document.getElementById("show-me");
+    show.style.display = 'block';
+
+    const name = document.getElementById("name");
+    const inputValue = name.value;
+    const h1 = document.querySelector("h1");
+    h1.textContent = "Hello, " + inputValue;
+    console.log(inputValue);
+    
 }
+
