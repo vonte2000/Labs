d74fb249a9d3e00df2f5b637a359be8128bd4354
DeVonte Gooden <dg208804@gmail.com>
2024-12-15 16:05:32 -0500

    properly linked js file to project

---
+++ Ready-Set-Action/index.js
@@ -0,0 +1,7 @@
+const main = () => {
+    const pGrow = document.getElementById("grow-me")
+    pGrow.classList.add("big")
+
+    const pShrink = document.getElementById("shrink-me")
+    pShrink.classList.remove("big")
+}
