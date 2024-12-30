be8f998b5903ec4d8d7b46c4f8e5b956cefdc4f6
DeVonte Gooden <dg208804@gmail.com>
2024-12-30 09:45:34 -0500

    started working on extended challenge for heating lab

--- Heating_Cooling/index.js
+++ Heating_Cooling/index.js
@@ -10,3 +10,27 @@ const checkTemp = (actualTemp) => {
     }
 }
 checkTemp(75)
+
+//etended challenge
+const convertTemp = (value, unit) => {
+    let result;
+    switch (unit) {
+        case "C": // C to F
+            result = (value * 9) / 5 + 32;
+            result = `${result}°F`;
+            break;
+        case "F": // F to C
+            result = ((value - 32) * 5) / 9;
+            result = `${result}°C`;
+            break;
+        case "K": //K to C
+            result = value - 273.15;
+            result = `${result}°C`;
+            break;
+        default:
+            result = "Invalid unit";
+        }
+        
+        return result;
+    }
+    console.log(convertTemp(20, "K"));
