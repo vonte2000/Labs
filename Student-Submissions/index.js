44133c0bbcb6612b8d828aa66054ec5d680345ee
DeVonte Gooden <dg208804@gmail.com>
2024-12-30 12:29:24 -0500

    completed the first section of lab work

---
+++ Student-Submissions/index.js
@@ -0,0 +1,21 @@
+const submissions = [
+    {name: "Jane", score: 96, passed: true},
+    {name: "Joe", score: 77, passed: true},
+    {name: "Jack", score: 59, passed: false},
+    {name: "Jill", score: 88, passed: true},   
+]
+
+const addSubmission = (array, name, score) => { // Fixing the function
+    const passed = score >= 60; // Determine passed
+    const newSubmission = { name, score, passed }; // Create the new submission object
+    array.push(newSubmission); // Add it to the array
+};
+
+addSubmission(submissions, "Vonte", 88)
+addSubmission(submissions, "Zae", 96)
+addSubmission(submissions, "Finess", 59)
+console.log(submissions);
+
+const deleteSubmissionsByIndex = () => {
+    
+}
